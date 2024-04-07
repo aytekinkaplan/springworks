7e87caf9a5a2370ab4b88e25cd2434540c3106c6
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-07 08:08:26 +0300

    Initial commit

---
+++ src/test/java/com/springworks/springworks/SpringworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springworks.springworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
