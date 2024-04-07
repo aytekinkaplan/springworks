40cc0f4a2255cb1c574f5545a00608e001822a87
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-07 08:08:59 +0300

    New commit

--- src/main/java/com/springworks/springworks/SpringworksApplication.java
+++ src/main/java/com/springworks/springworks/SpringworksApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringworksApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringworksApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringworksApplication.class, args);
+    }
 
 }
